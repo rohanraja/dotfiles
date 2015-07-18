f72ddb6b5a0995991e6502b9fe069937c2406d82
Rohan Raja <rohanraja9@gmail.com>
2015-07-18 13:55:04 +0530

    Fixed netspeed. Now should work with linux too

--- zsh/netspeed.sh
+++ zsh/netspeed.sh
@@ -1,21 +1,37 @@
 #!/bin/zsh -f
+
+ostype() { echo $OSTYPE | tr '[A-Z]' '[a-z]'; }
+
+export SHELL_PLATFORM='unknown'
+
+case "$(ostype)" in
+	*'linux'*	) SHELL_PLATFORM='linux'	;;
+	*'darwin'*	) SHELL_PLATFORM='osx'		;;
+	*'bsd'*		) SHELL_PLATFORM='bsd'		;;
+esac
+
+shell_is_linux() { [[ $SHELL_PLATFORM == 'linux' || $SHELL_PLATFORM == 'bsd' ]]; }
+shell_is_osx()   { [[ $SHELL_PLATFORM == 'osx' ]]; }
+shell_is_bsd()   { [[ $SHELL_PLATFORM == 'bsd' || $SHELL_PLATFORM == 'osx' ]]; }
+
 netspeed() {
   sleeptime="0.5"
   # if True then
+	if shell_is_osx; then
     iface="en1"
     RXB=$(netstat -i -b | grep -m 1 $iface | awk '{print $7}')
     TXB=$(netstat -i -b | grep -m 1 $iface | awk '{print $10}')
     sleep "$sleeptime"
     RXBN=$(netstat -i -b | grep -m 1 $iface | awk '{print $7}')
     TXBN=$(netstat -i -b | grep -m 1 $iface | awk '{print $10}')
-  # else
-    # iface="eth0"
-    # RXB=$(</sys/class/net/"$iface"/statistics/rx_bytes)
-    # TXB=$(</sys/class/net/"$iface"/statistics/tx_bytes)
-    # sleep "$sleeptime"
-    # RXBN=$(</sys/class/net/"$iface"/statistics/rx_bytes)
-    # TXBN=$(</sys/class/net/"$iface"/statistics/tx_bytes)
-  # fi
+  else
+    iface="eth0"
+    RXB=$(</sys/class/net/"$iface"/statistics/rx_bytes)
+    TXB=$(</sys/class/net/"$iface"/statistics/tx_bytes)
+    sleep "$sleeptime"
+    RXBN=$(</sys/class/net/"$iface"/statistics/rx_bytes)
+    TXBN=$(</sys/class/net/"$iface"/statistics/tx_bytes)
+  fi
   RXDIF=$(echo "$((RXBN - RXB)) / 1024 / ${sleeptime}" | bc )
   TXDIF=$(echo "$((TXBN - TXB)) / 1024 / ${sleeptime}" | bc )
 
