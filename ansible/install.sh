0bf7986ba8151ada795e0cbcde7d93afd2fa4ddc
Rohan <roraja@microsoft.com>
2023-07-29 16:36:25 +0530

    r

--- ansible/install.sh
+++ ansible/install.sh
@@ -4,8 +4,8 @@
 if [[ $(uname -s) == "Linux" ]]; then
     if command -v apt-get &>/dev/null; then
         sudo apt-get update
-        sudo apt-get install -y software-properties-common
-        sudo apt-add-repository --yes --update ppa:ansible/ansible
+        # sudo apt-get install -y software-properties-common
+        # sudo apt-add-repository --yes --update ppa:ansible/ansible
         sudo apt-get install -y ansible
     elif command -v yum &>/dev/null; then
         sudo yum install -y epel-release
