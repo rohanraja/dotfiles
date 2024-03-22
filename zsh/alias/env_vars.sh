c10c09acafa5d5fd8e9cbaa4842f03c0159a7343
Rohan Raja <roraja@microsoft.com>
2024-03-22 12:11:37 +0530

    added hosts

--- zsh/alias/env_vars.sh
+++ zsh/alias/env_vars.sh
@@ -1,3 +1,8 @@
 GOPATH=$HOME/go
 
 export SECOND_BRAIN_PATH="/Users/rohanraja/Library/Mobile Documents/iCloud~com~logseq~logseq/Documents/logseq-notes"
+
+export BEAST="rohan-linux@beast"
+export WIN11="rohan-linux-lappy@win11"
+
+export DOTFILES_PATH="$HOME/.dotfiles"
